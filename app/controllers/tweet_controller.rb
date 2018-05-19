23ff3557b397dca52cb0909c62b4931df37770a8
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-19 17:05:25 +0000

    Done.

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -8,11 +8,13 @@ class TweetController < ApplicationController
 
   get '/tweets/new' do
     redirect "/users/login" if session[:user_id] == nil
+    binding.pry
+    @tweet = Tweet.find_by(user_id: session[:user_id])
     erb :'/tweets/create_tweet'
   end
 
   post '/tweets' do
-    
+
   end
 
 end
